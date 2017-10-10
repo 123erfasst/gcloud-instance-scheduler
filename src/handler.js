0f5ac16c4fda5230c553ecdc35ae24cd50cbf2e4
David Künnen <dk@123erfasst.de>
2017-10-10 08:51:04 +0200

    Disable access for non Cron-Jobs

--- src/handler.js
+++ src/handler.js
@@ -3,6 +3,13 @@ const cloud = require('google-cloud');
 const handler = (req, res, method) => {
     const gce = cloud.compute();
 
+    if(!req.header('X-Appengine-Cron')) {
+        return res.status(400).json({
+            type: 'Unauthorized',
+            message: 'Only cron jobs have access'
+        });
+    }
+
     const {instances} = req.params;
     if(!instances) {
         return res.status(400).json({
@@ -10,6 +17,7 @@ const handler = (req, res, method) => {
             message: 'Please define at least one instance'
         });
     }
+
     Promise.all(
         instances.split('|').map(
             name => gce
--- test/app.test.js
+++ test/app.test.js
@@ -13,10 +13,32 @@ describe('handler', function () {
             stop: stopSpy
         })})
     });
+
+    it('should throw error if no cron job', function () {
+        let res = httpMocks.createResponse();
+        let req = httpMocks.createRequest({
+            method: 'GET',
+            url: '/start'
+        });
+
+        handler(req, res, 'start');
+
+        expect(res.statusCode).to.equal(400);
+        expect(res._isJSON()).to.equal(true);
+        expect(JSON.parse(res._getData())).to.deep.equal({
+            type: 'Unauthorized',
+            message: 'Only cron jobs have access'
+        })
+    });
+
+
     it('should throw error if no instances defined', function () {
         let res = httpMocks.createResponse();
         let req = httpMocks.createRequest({
             method: 'GET',
+            headers: {
+                ['X-Appengine-Cron']: true
+            },
             url: '/start'
         });
 
@@ -35,6 +57,9 @@ describe('handler', function () {
         let req = httpMocks.createRequest({
             method: 'GET',
             url: '/start/zone1:instance1',
+            headers: {
+                ['X-Appengine-Cron']: true
+            },
             params: {
                 instances: 'zone1:instance1'
             }
